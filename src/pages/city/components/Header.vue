1c455c74706f04cca5c95539f34ae38bf67e621c
zhy <1076843792@qq.com>
2018-06-06 11:55:44 +0800

    add city

---
+++ src/pages/city/components/Header.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="header">
+   <router-link to="/">
+      <span class="iconfont header-back">&#xe624;</span>
+   </router-link>
+    城市选择
+  </div>
+</template>
+<script>
+export default {
+  name: 'CityHeader'
+}
+</script>
+<style lang="stylus" scoped>
+   @import '..\..\..\assets\styles\varibles.styl'
+  .header
+    position: relative
+    overflow: hidden
+    height: $headerHeight
+    line-height: $headerHeight
+    text-align: center
+    color: #fff
+    background: $bgColor
+    font-size: .32rem
+    .header-back
+      position: absolute
+      top:0
+      left: 0
+      width: .64rem
+      text-align: center
+      font-size: .4rem
+      color: #fff
+</style>

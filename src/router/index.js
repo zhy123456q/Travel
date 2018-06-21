da562b5d30dcdd798c33c4c1baa490936f3c1e58
zhy <1076843792@qq.com>
2018-06-21 14:33:51 +0800

    add

--- src/router/index.js
+++ src/router/index.js
@@ -20,8 +20,9 @@ export default new Router({
       path: '/detail/:id',
       name: 'Detail',
       component: Detail
-    }],
-    scrollBehavior (to, from, savedPosition) {
-      return { x:0, y: 0}
     }
+  ],
+  scrollBehavior (to, from, savedPosition) {
+    return {x: 0, y: 0}
+  }
 })

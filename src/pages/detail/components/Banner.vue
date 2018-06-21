8cf7438885cc9a9bba9bb23fa3bfcfdb5829dc87
zhy <1076843792@qq.com>
2018-06-21 15:00:05 +0800

    add animation

--- src/pages/detail/components/Banner.vue
+++ src/pages/detail/components/Banner.vue
@@ -14,16 +14,19 @@
           </div>
         </div>
     </div>
-    <common-gallary
-      :imgs="gallaryImgs"
-      v-show="showGallary"
-      @close="handleShowGallary"
-    >
-    </common-gallary>
+    <fade-animation>
+      <common-gallary
+        :imgs="gallaryImgs"
+        v-show="showGallary"
+        @close="handleShowGallary"
+      >
+      </common-gallary>
+    </fade-animation>
   </div>
 </template>
 <script>
 import CommonGallary from 'common/gallary/Gallary'
+import FadeAnimation from 'common/fade/FadeAnimation'
 export default {
   name: 'DetailBanner',
   props: {
@@ -37,7 +40,8 @@ export default {
     }
   },
   components: {
-    CommonGallary
+    CommonGallary,
+    FadeAnimation
   },
   methods: {
     handleShowGallary () {
